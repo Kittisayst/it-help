8c8c845d048c99cee3ae2f60e87bb09ea577e9a1
Kittisay SENGTHONG <85366173+Kittisayst@users.noreply.github.com>
2026-02-26 13:05:26 +0700

    Update collectors init

--- agent/collectors/__init__.py
+++ agent/collectors/__init__.py
@@ -7,7 +7,7 @@ from .processes import collect_processes
 from .event_log import collect_event_logs
 from .software import collect_software
 from .antivirus import collect_antivirus
-from .printers import collect_printers
+from .printers import collect_printers, collect_print_history
 from .windows_license import collect_windows_license
 from .office_license import collect_office_license
 from .startup import collect_startup
@@ -15,7 +15,6 @@ from .shared_folders import collect_shared_folders
 from .usb_devices import collect_usb_devices
 from .windows_update import collect_windows_update
 from .services import collect_services
-from .printers import collect_services as _, collect_print_history  # Fix: use the history function
 from .active_apps import start_tracking, collect_app_usage
 
 __all__ = [
